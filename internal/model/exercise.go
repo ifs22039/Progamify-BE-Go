3955a8dfa9684c7629be355cf18c03212281cf5d
Boy Sitorus <sitorusboy0123@gmail.com>
2025-02-27 20:42:41 +0700

    remove env from git

--- internal/model/exercise.go
+++ internal/model/exercise.go
@@ -6,5 +6,5 @@ type Exercise struct {
 	gorm.Model
 	Title     string       `json:"title" gorm:"size:255;not null"`
 	LessonID  uint         `json:"-"`
-	Questions []ExQuestion `json:"questions,omitempty" gorm:"foreignKey:ExerciseID"`
+	Questions []ExQuestion `json:"questions" gorm:"foreignKey:ExerciseID"`
 }
