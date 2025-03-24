3aed27bd6c8b5c116b4ada439bd2d399912f03d9
Boy Sitorus <sitorusboy0123@gmail.com>
2025-03-24 14:51:56 +0700

    add have avatar when create user

--- internal/repository/discussion_repository.go
+++ internal/repository/discussion_repository.go
@@ -26,7 +26,7 @@ func (r *discussionRepository) AddReply(reply *model.DiscReply) error {
 
 func (r *discussionRepository) FindById(discussionID uint) (*model.Discussion, error) {
 	var discussion model.Discussion
-	err := r.db.Preload("Replies.DetailUser.Avatar").First(&discussion, discussionID).Error
+	err := r.db.Preload("User.Avatar").Preload("Replies.DetailUser.Avatar").First(&discussion, discussionID).Error
 
 	if err != nil {
 		// Return the error early if the record is not found
